e24edbecd6015af9ca64d62f737edac6f5bbd81b
Gonzalo Dicosimo <gdicosimo@alumnos.exa.unicen.edu.ar>
2022-06-07 21:32:02 -0300

    TPMT-47 Se implemento la clase login en el apartado del paciente, con la interfaz de solicitud de dni, verificando la existencia del mismo en la base de datos y la solicitud de datos personales.

--- ProjectJava/src/TPE/Main.java
+++ ProjectJava/src/TPE/Main.java
@@ -18,6 +18,21 @@ public class Main {
 		// Logueo de usuario
 		Login.loguear(usuario, clinica);
 
+		pacientes.agregarClinica(clinica); //lee el archivo que guarda la info. de pacientes
+		medicos.agregarClinica(clinica); //lee el archivo que guarda la info. de medicos
+		secretarias.agregarClinica(clinica); //lee el archivo que guarda la info. de secretarias
+		turnos.agregarClinica(clinica); // lee el archivo que guarda la info. de turnos
+			
+		//Codigo
+		System.out.println("Pacientes:");
+		System.out.println(clinica.getPacientes());
+		System.out.println("Medico:");
+		System.out.println(clinica.getMedico());
+		System.out.println("Secretaria:");
+		System.out.println(clinica.getSecretaria());
+		System.out.println("Turnos:");
+		System.out.println(clinica.getTurnos());
+		
 		//Guardado de datos
 		WriteCSV archivoPacientes = new WritePacientes(clinica);
 		archivoPacientes.generarArchivoCSV(pacientes.getCsvFile());
